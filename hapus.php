b5ab0acf4d66632aad200ab3ed090dc5d7f068f3
IDGunawan411 <63904116+IDGunawan411@users.noreply.github.com>
2020-12-17 20:10:26 +0700

    all

--- hapus.php
+++ hapus.php
@@ -1,8 +1,12 @@
 <?php
 include "koneksi.php";
-    $strow = mysqli_query($koneksi,"SELECT * FROM biodata");
+    $stb = mysqli_query($koneksi,"SELECT * FROM biodata");
+    while($tb  = mysqli_fetch_array($stb)){
+        mysqli_query($koneksi,"DELETE FROM biodata WHERE id='$tb[id]'");
+    }
+    $strow = mysqli_query($koneksi,"SELECT * FROM row");
     while($trow  = mysqli_fetch_array($strow)){
-        mysqli_query($koneksi,"DELETE FROM biodata WHERE id='$trow[id]'");
+        mysqli_query($koneksi,"DELETE FROM row WHERE id='$trow[id]'");
     }
     header("location:input.php");
 ?>
