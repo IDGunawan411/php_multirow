75492b5b7b072b113f2eb9ea13822c0173b7c287
IDGunawan411 <63904116+IDGunawan411@users.noreply.github.com>
2020-12-18 08:43:13 +0700

    sa

--- post_row.php
+++ post_row.php
@@ -6,8 +6,8 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
     $total_row = $trow['total_row'];
 
     for($i = 1; $i <= $total_row;$i++){
-        $nama = $_POST["nama$i"];
-        $alamat = $_POST["alamat$i"];
+        $nama       = $_POST["nama$i"];
+        $alamat     = $_POST["alamat$i"];
         if($_POST["nama$i"]==!NULL && $_POST["alamat$i"]==!NULL){
             mysqli_query($koneksi,"INSERT INTO `biodata` (`id`, `nama`, `alamat`) VALUES (NULL, '$nama', '$alamat')");
         }
